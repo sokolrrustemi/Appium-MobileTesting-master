5e2b173b94a1187d2feb2877eb5b55a31d9e5e98
sokolrrustemi <141067973+sokolrrustemi@users.noreply.github.com>
2024-01-09 15:54:22 +0100

    Add files via upload

---
+++ src/test/java/utils/HooksWeb.java
@@ -0,0 +1,33 @@
+package utils;
+
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeMethod;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class HooksWeb {
+
+    public static AndroidDriver androidDriver;
+    static DesiredCapabilities capabilities = new DesiredCapabilities();
+    @BeforeMethod
+    public void setUp() {
+
+        try {
+            capabilities.setCapability("deviceName", ConfigReader.getProperty("deviceName"));
+            capabilities.setCapability("platformName", ConfigReader.getProperty("platformName"));
+            capabilities.setCapability("platformVersion", ConfigReader.getProperty("platformVersion"));
+            capabilities.setCapability("automationName", ConfigReader.getProperty("automationName"));
+            capabilities.setCapability("browserName","chrome");
+            androidDriver = new AndroidDriver(new URL("http://127.0.0.1:4723"), capabilities);
+            androidDriver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        } catch (MalformedURLException e) {
+            System.err.println("Malformed URL Exception: " + e.getMessage());
+
+        }
+    }
+
+
+}
